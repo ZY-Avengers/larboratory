18410ac0b5dfdbee9022dabc60964f93252526d8
meifans <916109363@qq.com>
2016-09-21 23:45:54 +0800

    add application function

---
+++ src/main/java/com/github/Avengers/larboratory/Application.java
@@ -0,0 +1,25 @@
+package com.github.Avengers.larboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Meifans on 2016/9/21.
+ */
+@RestController
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/api/{who}/{toSay}")
+    public String sample(@PathVariable String toSay, @PathVariable String who) {
+        return who + "," + " I " + toSay;
+    }
+
+}
