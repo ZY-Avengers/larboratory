bed3d28d5251c52acee061f1eaaa36c9e02604d7
赵鹏飞 <pengfeizhao9@creditease.cn>
2016-10-24 16:35:21 +0800

    fix forEach

--- src/main/java/com/github/Avengers/larboratory/SimpleForEach.java
+++ src/main/java/com/github/Avengers/larboratory/SimpleForEach.java
@@ -2,29 +2,32 @@ package com.github.Avengers.larboratory;
 
 import java.util.Iterator;
 
-public class SimpleForEach implements Iterable<String>{
+public class SimpleForEach implements Iterable<String> {
 
-	private String[] str = {"tianlelu","why","write","this"};
-	
-	public Iterator<String> iterator() {		
-		return new Iterator<String>(){
-			private int index = 0;
-			public boolean hasNext(){
-				return index < str.length;
-			}
-			public String next() {
-				return null;//不知道怎么写了
-			}
-			public void remove() {				
-			}
-		};
-	}
-	
+	private String[] str = {"tianlelu", "why", "write", "this"};
+
 	public static void main(String[] args) {
 		SimpleForEach simpleForEach = new SimpleForEach();
-		for(String s: simpleForEach){
+		for (String s : simpleForEach) {
 			System.out.println(s);
 		}
+	}
+
+	public Iterator<String> iterator() {
+		return new Iterator<String>() {
+
+			private int index = 0;
+
+			public boolean hasNext() {
+				return index < str.length;
+			}
+
+			public String next() {
+				return str[index++];//不知道怎么写了
+			}
+		};
+
+
 	}
 }
 
