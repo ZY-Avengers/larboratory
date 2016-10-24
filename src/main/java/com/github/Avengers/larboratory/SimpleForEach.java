3644788819af525bcdc124c42508cee8cbe09ff6
赵鹏飞 <pengfeizhao9@creditease.cn>
2016-10-24 17:17:42 +0800

    add demo of list forEach

--- src/main/java/com/github/Avengers/larboratory/SimpleForEach.java
+++ src/main/java/com/github/Avengers/larboratory/SimpleForEach.java
@@ -1,37 +1,47 @@
 package com.github.Avengers.larboratory;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class SimpleForEach implements Iterable<String> {
 
 	private String[] str = {"tianlelu", "why", "write", "this"};
 
 	public static void main(String[] args) {
-		SimpleForEach simpleForEach = new SimpleForEach();
-		for (String s : simpleForEach) {
+		SimpleForEach strings = new SimpleForEach();
+
+		strings.eachSimple(strings);
+		strings.eachDemo();
+
+	}
+
+	private void eachSimple(SimpleForEach strings) {
+		for (String s : strings)
 			System.out.println(s);
-		}
+	}
+
+	private void eachDemo() {
+		for (String s : new Demo())
+			System.out.println(s);
 	}
 
 	public Iterator iterator() {
 		return new Iterator() {
-
 			private int index = 0;
+			public boolean hasNext() {return index < str.length;}
 
-			public boolean hasNext() {
-				return index < str.length;
-			}
+			public String next() {return str[index++];}//不知道怎么写了
 
-			public String next() {
-				return str[index++];//不知道怎么写了
-			}
-
-			public void remove() {
-				str = null;
-			}
-
+			public void remove() {str = new String[4];}
 		};
+	}
 
+	class Demo implements Iterable<String> {
+
+		private List elements = Arrays.asList("tianlelu", "why", "write", "this");
+
+		public Iterator<String> iterator() {return elements.iterator();}
 	}
 }
 
