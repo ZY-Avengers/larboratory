ce2aa6206f026259144d84c787fb522acede61cf
meifans <916109363@qq.com>
2016-11-21 22:35:25 +0800

    add java8

--- src/main/java/com/github/Avengers/larboratory/Application.java
+++ src/main/java/com/github/Avengers/larboratory/Application.java
@@ -1,10 +1,14 @@
 package com.github.Avengers.larboratory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.rowset.spi.XmlReader;
 
 /**
  * Created by Meifans on 2016/9/21.
@@ -13,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 
+	@Autowired
+	private XmlReader reader;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping(method = RequestMethod.GET,path = "/")
 	public String sample() {
 		return MicroService.sayYes();
 	}
@@ -47,4 +54,7 @@ public class Application {
 		}
 
 	}
+
+
+
 }
---
+++ src/test/java/com/github/Avengers/larboratory/java8/SampleStreamTest.java
@@ -0,0 +1,12 @@
+package com.github.Avengers.larboratory.java8;
+
+import org.junit.Test;
+
+/**
+ * Created by Meifans on 2016/10/21.
+ */
+@Test
+public class SampleStreamTest {
+
+
+}
